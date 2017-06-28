a0b2b573116e91eb2ab7e647748403d1761a8945
Duc Tan Dang <ductan.dang@googlemail.com>
2017-06-28 23:15:38 +0200

    init css, update logic

--- js/logic.js
+++ js/logic.js
@@ -13,14 +13,55 @@ var PokeApp = (function() {
     DOM.$solutionInput = $(".solution__input");
     DOM.$resetButton = $(".keyboard__reset");
     DOM.$compareButton = $(".keyboard__compare");
+    DOM.$replayButton = $(".start__replay");
+    DOM.$resetButton = $(".start__reset");
   }
 
   // bind events
   function bindEvents() {
-    DOM.$generateButton.on("click", generateMemory);
+    DOM.$generateButton.on("click", _onStartButton);
     DOM.$addKey.on("click", addToInput);
-    DOM.$resetButton.on("click", resetInputField);
+    DOM.$resetButton.on("click", _onResetButton);
     DOM.$compareButton.on("click", compareInput);
+    DOM.$replayButton.on("click", _onReplayButton);
+  }
+
+  // set element click function
+  function _onStartButton() {
+    if(DOM.$generateButton.data("state") == "start") {
+      generateMemory();
+      setCheckButton();
+    }
+    else {
+      compareInput();
+    }
+  }
+
+  function _onReplayButton() {
+    replayPuzzle();
+  }
+
+  function _onResetButton() {
+    generateMemory();
+  }
+
+
+  // functions
+
+  function resetPuzzle() {
+    generateMemory();
+  }
+
+  function setCheckButton() {
+    var curState = DOM.$generateButton.data("state");
+
+    if(curState == "start") {
+      DOM.$generateButton.data("state", "replay").text("Check");
+    }
+  }
+
+  function replayPuzzle() {
+    console.log(curPuzzle);
   }
 
 
@@ -33,7 +74,7 @@ var PokeApp = (function() {
     var min = 0
       , max = 5
       , puzzleArray = []
-      , numberOfPuzzles = 4;
+      , numberOfPuzzles = 3;
 
     for (var i = 0; i < numberOfPuzzles; i++) {
       puzzleArray.push( Math.floor(Math.random() * (max - min + 1)) + min );
